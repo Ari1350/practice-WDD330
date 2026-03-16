c8e05367ae153a776667268149a8522710ddef41
Guadalupe <ariasperaltag@gmail.com>
2026-03-15 20:15:46 -0400

    Terminada actividad de detalles de producto dinámicos

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,12 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
+console.log("¡Archivo product.js cargado correctamente!");
+
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  const carItems = getLocalStorage('so-cart') || [];
-  carItems.push(product);
-  setLocalStorage('so-cart', carItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
